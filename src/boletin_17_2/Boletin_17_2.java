8412f68ebef92d1690ec6e5edb8bdeb84c89ec1c
jalvarezotero <you@example.com>
2019-01-23 12:03:29 +0100

    Parte 2 del boletin acabado

---
+++ src/boletin_17_2/Boletin_17_2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_17_2;
+
+import boletin_17_1.Gato;
+import boletin_17_1.Mamífero;
+import boletin_17_1.Papagaio;
+
+/**
+ *
+ * @author jalvarezotero
+ */
+public class Boletin_17_2 {
+    
+        Aves av;
+        Avestruza avs =new Avestruza();
+        Morcego mor =new Morcego();
+        Papagaio pap =new Papagaio();
+        Mamífero mam = new Mamífero();
+        Felino fel = new Felino();
+        Gato gat = new Gato();
+        Tigre tig = new Tigre();
+
+   
+}
---
+++ src/boletin_17_2/Morcego.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_17_2;
+
+/**
+ *
+ * @author jalvarezotero
+ */
+class Morcego {
+      public void caminar() {
+    }
+
+    public void voar() {
+    }
+}
